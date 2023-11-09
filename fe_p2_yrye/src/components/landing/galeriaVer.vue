8d15e6872208c8dce7bac96406fb74d4f42fef4c
ynvYauneEnovore <yovanuxf@gmail.com>
2023-11-09 12:33:55 -0400

    parcialsd-fotosc

--- fe_p2_yrye/src/components/landing/galeriaVer.vue
+++ fe_p2_yrye/src/components/landing/galeriaVer.vue
@@ -4,40 +4,37 @@
  
     <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="/images/1.jpeg" alt="">
+            <img class="h-auto max-w-full rounded-lg" src="/images/1.jpg" alt="">
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="/images/2.jpeg" alt="">
+            <img class="h-auto max-w-full rounded-lg" src="/images/2.jpg" alt="">
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="/images/3.jpeg" alt="">
+            <img class="h-auto max-w-full rounded-lg" src="/images/3.jpg" alt="">
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="/images/4.jpeg" alt="">
+            <img class="h-auto max-w-full rounded-lg" src="/images/4.jpg" alt="">
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="/images/5.jpeg" alt="">
+            <img class="h-auto max-w-full rounded-lg" src="/images/5.jpg" alt="">
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="/images/6.jpeg" alt="">
+            <img class="h-auto max-w-full rounded-lg" src="/images/7.jpg" alt="">
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="/images/7.jpeg" alt="">
+            <img class="h-auto max-w-full rounded-lg" src="/images/8.jpg" alt="">
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="/images/8.jpeg" alt="">
+            <img class="h-auto max-w-full rounded-lg" src="/images/9.jpg" alt="">
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="/images/9.jpeg" alt="">
+            <img class="h-auto max-w-full rounded-lg" src="/images/10.jpg" alt="">
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="/images/10.jpeg" alt="">
+            <img class="h-auto max-w-full rounded-lg" src="/images/11.jpg" alt="">
         </div>
         <div>
-            <img class="h-auto max-w-full rounded-lg" src="/images/11.jpeg" alt="">
-        </div>
-        <div>
-            <img class="h-auto max-w-full rounded-lg" src="/images/12.jpeg" alt="">
+            <img class="h-auto max-w-full rounded-lg" src="/images/12.jpg" alt="">
         </div>
     </div>
     </div>    
