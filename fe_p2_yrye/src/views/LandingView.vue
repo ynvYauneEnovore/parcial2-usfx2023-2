296564cbc540590c73166cd387456072d80f27d4
ynvYauneEnovore <yovanuxf@gmail.com>
2023-11-09 15:21:46 -0400

    parcialsd- corecion landig

--- fe_p2_yrye/src/views/LandingView.vue
+++ fe_p2_yrye/src/views/LandingView.vue
@@ -1,15 +1,10 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { onMounted } from 'vue';
 
 import { initFlowbite } from 'flowbite'
 import 'flowbite';
-import Tema from '@/components/global/tema.vue';
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
-import Serie from '@/components/Series/SerieList.vue';
-import Galeria from '@/components/landing/galeriaVer.vue';
-
-
 
 onMounted(() => {
   initFlowbite();
